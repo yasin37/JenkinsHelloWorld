a89976a2fdb5534c60d8980adc52de5e60532fdf
t00277373 <turker.yasin.yazkan@huawei.com>
2020-01-12 15:34:35 +0300

    WEB HOOK BRO

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("HELLO UNIVERSE");
+        System.out.println("TRYING WEBHOOK");
     }
 
 }
